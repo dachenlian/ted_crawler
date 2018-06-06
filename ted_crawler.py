b815d7430db93326487e763077664143f93cda87
dachenlian <cclemon.lian@gmail.com>
2018-06-06 08:14:23 +0800

    it works now

--- ted_crawler.py
+++ ted_crawler.py
@@ -1,9 +1,11 @@
 import re
 import json
 from pathlib import Path
+from time import sleep
 
 import requests
 from pyquery import PyQuery as pq
+from fake_useragent import UserAgent
 
 
 LAST_PAGE_CSS = "#browse-results > div.results__pagination > div > a:nth-child(13)"
@@ -13,14 +15,18 @@ TALK_DETAILS_REGEX = re.compile(r"(?P<name>.+)\n(?P<title>.+)\nPosted (?P<post_d
 LANGUAGES_CSS = "#shoji > div.shoji__door > div > div.main.pages-main > div > div > div > " \
                       "div.col-lg-9.pages-content > div.row.row-xs-2up.row-sm-3up.row-lg-4up > div"
 
+ua = UserAgent()
+HEADERS = {'user-agent': ua.chrome}
+
 
 class TedCrawler:
     def __init__(self):
-        self.current_page = 1
-        self.last_page = self.get_last_page()
+        self.current_page_no = 0
         self.base_url = "https://www.ted.com/"
         self.page_url = "talks?page={}"
-        self.transcript_url_base = "transcript.json?language={}"
+        self.transcript_url_base = "/transcript.json?language={}"
+        self.current_page = self.get_next_page()
+        self.last_page = self.get_last_page()
         self.languages = self.get_languages()
         self.bookmark = self.get_bookmark()
         self.bookmark_has_run = False
@@ -28,7 +34,7 @@ class TedCrawler:
 
     def get_languages(self):
         url = self.base_url + "participate/translate/our-languages"
-        d = pq(requests.get(url).text)
+        d = pq(requests.get(url, headers=HEADERS).text)
         languages = d(LANGUAGES_CSS)
         language_dict = {}
         for language in languages:
@@ -39,40 +45,49 @@ class TedCrawler:
         return language_dict
 
     def get_last_page(self):
-        last_page = pq(LAST_PAGE_CSS).text()
+        last_page = self.current_page(LAST_PAGE_CSS).text()
         return int(last_page)
 
     def get_next_page(self):
-        r = requests.get(self.base_url + self.page_url.format(self.current_page)).text
+        self.current_page_no += 1
+        r = requests.get(self.base_url + self.page_url.format(self.current_page_no), headers=HEADERS).text
         next_page = pq(r)
-        yield next_page
-        self.current_page += 1
+        print(f"Checking page {self.current_page_no}...")
+        self.current_page = next_page
+        return next_page
 
-    def get_talk_list(self, page):
-        print(f"Checking page {self.current_page}...")
-        talk_list = page(TALK_LIST_CSS)
-        yield talk_list
+    def get_talk_list(self):
+        talk_list = self.current_page(TALK_LIST_CSS)
+        return talk_list
 
     def get_talk_details(self, talk):
-        talk_link = pq(talk).find('a').attr('href')
+        talk = pq(talk)
+        talk_link = talk.find('a').attr('href')
         results = {'talk_link': talk_link, 'transcripts': {}}
-        details = TALK_DETAILS_REGEX.search(talk)
-        yield results.update(details.groupdict())
+        details = TALK_DETAILS_REGEX.search(talk.text())
+        results.update(details.groupdict())
+        return results
 
     def get_subtitles(self, talk, *args):
         transcript_url = self.base_url + talk['talk_link'] + self.transcript_url_base
+        if not args:
+            raise TypeError("Please choose at least one language.")
         for language in args:
             language_code = self.languages.get(language, None)
             if language_code:
-                transcript = requests.get(transcript_url.format(language_code)).json()
-                talk['transcripts'][language_code] = transcript
-            else:
-                transcript = requests.get(transcript_url.format(language)).json()
-                if transcript['status'] == '404':
-                    print(f"{language} not found.")
-                    continue
+                transcript = requests.get(transcript_url.format(language_code), headers=HEADERS).json()
 
-        yield talk
+            else:
+                language_code = language
+                transcript = requests.get(transcript_url.format(language_code), headers=HEADERS).json()
+
+            if transcript.get('status', None) == '404':
+                    print(f"{language} not found.")
+                    talk['transcripts'][language_code] = None
+                    continue
+            talk['transcripts'][language_code] = transcript
+
+        return talk
 
     def create_bookmark(self, talk):
         if not self.bookmark_has_run:
@@ -80,23 +95,44 @@ class TedCrawler:
                 d = {'bookmark': talk['title']}
                 print(f"Creating bookmark with title: {talk['title']}")
                 json.dump(d, fp, indent=4, ensure_ascii=False)
+        self.bookmark_has_run = True
 
     def get_bookmark(self):
         bookmark = Path("bookmark.json")
         if bookmark.is_file():
             print("Found bookmark.")
             with open('bookmark.json', 'r') as fp:
-                return json.load(fp)
+                bookmark = json.load(fp)
+                return bookmark['bookmark']
         else:
             print("Bookmark not found.")
 
-    def write_to_json(self, path_name='transcripts.json'):
-        with open(path_name) as fp:
+    def write_to_json(self, path_name='ted_transcripts.json'):
+        with open(path_name, 'w', encoding='utf8') as fp:
             json.dump(self.transcripts, fp, indent=4, ensure_ascii=False)
+            # self.transcripts = {}
 
 
 if __name__ == "__main__":
     crawler = TedCrawler()
+    counter = 1
+    while crawler.current_page_no < crawler.last_page:
+        crawler.get_next_page()
+        talk_list = crawler.get_talk_list()
+        for talk in talk_list:
+            sleep(3)
+            talk_details = crawler.get_talk_details(talk)
+            # if crawler.bookmark == talk_details['title']:
+            #     print("Encountered old video. Quitting...")
+            #     break
+            crawler.create_bookmark(talk_details)
+            talk_details = crawler.get_subtitles(talk_details, 'zh-cn', 'zh-tw')
+            crawler.transcripts[talk_details['title']] = talk_details
+        # if crawler.current_page_no % 10 == 0:
+        #     crawler.write_to_json(path_name=f"TED_transcripts_{counter}.json")
+        #     counter += 1
+
+    crawler.write_to_json(path_name=f"TED_transcripts_{counter}.json")
 
 
 
